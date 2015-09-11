49ebb28b6e54496b9d79a9085b07f6f236ad7e3c
Goran Milovanović <goran@effectiva.hr>
2015-09-11 17:47:05 +0200

    group blocktypes

--- app/scripts/collections/block_templates.js
+++ app/scripts/collections/block_templates.js
@@ -1,13 +1,35 @@
-define(['app', 'collection', 'models/block_template'], function(App, Collection, BlockTemplate){
+define(['underscore', 'app', 'collection', 'models/block_template'], function(_, App, Collection, BlockTemplate){
   'use strict';
 
   return Collection.extend({
     cached: true,
+    model: BlockTemplate,
 
     url: function(){
       return App.env.base_url +  'block_types.json';
     },
-    model: BlockTemplate
+
+    by_group: function(){
+      if(this._by_group){ return; }
+      this._by_group = _.groupBy(this.models, function(item){return item.get('group');});
+      return this._by_group;
+    },
+
+    /*by_group_and_order: function(){
+      console.log(this._by_group_and_order);
+      if(this._by_group_and_order) { return; }
+
+      this._by_group_and_order = [];
+
+      _.map(this.by_group(), function(items, id){
+        console.log(id);
+        this._by_group_and_order[id] = _.groupBy(items, function(item){return item.get('order');});
+      }, this);
+
+      return this._by_group_and_order;
+
+    },*/
+
   });
 
 });
--- app/scripts/views/block_templates.js
+++ app/scripts/views/block_templates.js
@@ -3,7 +3,21 @@ define(['underscore', 'view', './block_template', 'app', './dnd'], function(_, V
 
   return View.extend(Dnd).extend({
     sort_element: '[data-zone]',
-    ViewItem: ViewBlockTemplate
+    template: 'block_templates/items',
+    //ViewItem: ViewBlockTemplate,
+
+    initialize: function(){
+      this._super('initialize', arguments);
+
+      var block_types = this.collection.by_group();
+
+      this.context.simple_blocks = block_types[0];
+      this.context.group_blocks = block_types[1];
+      this.context.container_blocks = block_types[2];
+      this.context.custom_blocks = block_types[3];
+
+      return this;
+    }
   });
 
 });
