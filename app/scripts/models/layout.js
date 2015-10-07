d37592b9e0f352eaf07816c426085b7074444a25
Goran Milovanović <goran@effectiva.hr>
2015-10-07 17:29:55 +0200

    layout zones

--- app/scripts/models/layout.js
+++ app/scripts/models/layout.js
@@ -6,23 +6,16 @@ define(['underscore', 'model', 'app'], function(_, Model, App){
 
     path: 'layouts',
 
-    parse: function (response) {
-      response.zones = response.zones ? JSON.parse(response.zones) : [];
-      response.positions = response.positions ? JSON.parse(response.positions) : [];
-      return response;
-    },
-
     toJSON: function(options){
       options || (options = {});
       var json = Model.prototype.toJSON.apply(this, arguments);
       if(!options.parse){return json;}
-      json.zones = JSON.stringify(json.zones);
       json.positions = JSON.stringify(json.positions);
       return json;
     },
 
     inherited_zones: function(){
-      return _.where(App.g.layout.get('zones'), {type: 2});
+      return _.where(App.g.layout.get('zones'), {kind: 2});
     }
 
 
--- app/scripts/models/zone.js
+++ app/scripts/models/zone.js
@@ -8,16 +8,17 @@ define(['underscore', 'model'], function(_, Model){
     },
 
     type_name: function(){
-      return this.TYPES[this.get('type')];
+      return this.TYPES[this.get('kind')];
     },
 
     is_inherited: function(){
-      return this.get('type') === 2;
+      return this.get('kind') === 2;
     },
 
     should_accept: function(template_or_block){
       var accepts = this.get('accepts');
-      if(accepts === true){ return true; }
+      console.log(accepts.length);
+      if(accepts.length === 0){ return true; }
       var id = template_or_block.get('template_id') || template_or_block.id;
       return _.contains(accepts, id);
     }
