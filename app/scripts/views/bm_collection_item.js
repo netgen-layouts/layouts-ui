20f4dcc139be9d5e1f16994e1a0ba83f4c7aef53
Goran Martinjak <goran@netgen.hr>
2016-05-24 16:48:41 +0200

    collection items draggable sorting

--- app/scripts/views/bm_collection_item.js
+++ app/scripts/views/bm_collection_item.js
@@ -5,19 +5,16 @@ var Core = require('core');
 
 module.exports = Core.View.extend({
   template: 'bm_collection_item',
+  className: 'collection-item',
 
   initialize: function(){
     Core.View.prototype.initialize.apply(this, arguments);
     return this;
   },
 
-  events: {
-    click: '$move'
-  },
-
-  $move: function(){
+  $move: function(i){
     this.model.save({
-      position: parseInt(window.prompt('Position?'))
+      position: i
     },{
       via: 'move',
       url: this.model.url('move'),
--- app/scripts/views/bm_collection_items.js
+++ app/scripts/views/bm_collection_items.js
@@ -8,8 +8,20 @@ module.exports = Core.View.extend({
   initialize: function(){
     Core.View.prototype.initialize.apply(this, arguments);
     // this.listenTo(this.collection, 'move:success', this.render);
+    this.setup_dnd();
     return this;
   },
   ViewItem: BmCollectionItemView,
-  template: 'bm_collection_items'
+  template: 'bm_collection_items',
+  setup_dnd: function(){
+    this.$el.sortable({
+      handle: '.handle',
+      delay: 150,
+
+      stop: function(e, ui){
+        $(ui.item).data('_view').$move($(ui.item).index());
+      }
+
+    });
+  }
 });
