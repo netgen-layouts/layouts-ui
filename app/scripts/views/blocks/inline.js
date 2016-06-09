d3d410e335bdae3be434a3eedbbb5cdf5e159d07
Marko Zabcic <gm.marko@gmail.com>
2016-06-09 12:21:49 +0200

    Support textarea in inline elements

--- app/scripts/views/blocks/inline.js
+++ app/scripts/views/blocks/inline.js
@@ -21,10 +21,16 @@ module.exports = {
 
   $keyup: function (e) {
     var $target = $(e.target), name = $target.data('attr');
-    var value = $target.text().trim();
-    var $input = $('.sidebar input[name*="['+name+']"]');
-    $input.val(value);
-    this.debounced_save($input);
+    var value = $target.html().trim();
+    var $input_or_textarea = $('.sidebar [name*="['+name+']"]');
+    value = value.replace(/<br>/g, '\n');
+    value = value.replace(/&nbsp;/g, ' ');
+
+    if(!$input_or_textarea.length){
+      throw new Error('Inline element not found in sidebar form.')
+    }
+    $input_or_textarea.val(value);
+    this.debounced_save($input_or_textarea);
   },
 
   $save: function($input){
