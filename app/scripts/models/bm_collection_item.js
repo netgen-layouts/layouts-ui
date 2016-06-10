6f95ae93f8b4264ab53e3965d55bfd17dd1cea1f
Goran Martinjak <goran@netgen.hr>
2016-06-10 15:23:48 +0200

    remove item disabled when named collection

--- app/scripts/models/bm_collection_item.js
+++ app/scripts/models/bm_collection_item.js
@@ -6,6 +6,6 @@ module.exports = Core.Model.extend({
   path: 'collections/items',
 
   can_remove_item: function(){
-    return this.get('type') !== 2;
+    return this.get('type') !== 2 && this.collection.bm_collection.get('type') !== 2;
   },
 });
