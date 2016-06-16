a000cd69b62ecf6ad17b0c832f24f8519bf53ba6
Goran Martinjak <goran@netgen.hr>
2016-06-16 16:31:02 +0200

    layout name change functionality

--- app/scripts/views/header.js
+++ app/scripts/views/header.js
@@ -8,30 +8,53 @@ module.exports = Core.View.extend({
   template: 'header',
 
   events: {
-    'blur .layout-name > h1': 'set_name',
-    'keydown .layout-name > h1': 'keypress_events'
+    'submit form.js-layout': 'set_name',
+    'click .js-show-form': 'enter_editing',
+    'click .js-cancel': 'cancel_editing'
   },
 
   initialize: function(){
     Core.View.prototype.initialize.apply(this, arguments);
+    this.listenTo(this.model, 'save:success', this.after_save);
+
     this.render();
+
     return this;
   },
 
   render: function(){
     Core.View.prototype.render.apply(this, arguments);
-    this.$layout_name = this.$('.layout-name > h1');
+    this.$name_input = this.$('.js-name');
     return this;
   },
 
-  set_name: function(){
+  set_name: function(e){
+    e.preventDefault();
+    this.model.save(this.serialize().params, {method: "POST", patch: true});
   },
 
-  keypress_events: function(e){
-    if(e.keyCode === 13){
-      this.$layout_name.blur();
-      e.preventDefault();
-    }
+  enter_editing: function(){
+    this.context.editing = true;
+    this.render();
+    this.$('form.js-layout input[name="name"]').focus();
+
+    var strLength = this.$name_input.val().length * 2;
+    this.$name_input.focus();
+    this.$name_input[0].setSelectionRange(strLength, strLength);
+  },
+
+  exit_editing: function(){
+    this.context.editing = false;
+    this.render();
+  },
+
+  cancel_editing: function(e){
+    e.preventDefault();
+    this.exit_editing();
+  },
+
+  after_save: function(){
+    this.exit_editing();
   }
 
 });
