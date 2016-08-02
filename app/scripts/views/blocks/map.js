24eb7d98610e75052edb865770ab69e6cad178d7
Goran Martinjak <goran@netgen.hr>
2016-08-02 14:49:23 +0200

    google map js

--- app/scripts/views/blocks/map.js
+++ app/scripts/views/blocks/map.js
@@ -8,6 +8,20 @@ module.exports = Block.extend({
 
   //prevent_auto_render: true,
 
+  render: function() {
+    Block.prototype.render.apply(this,arguments);
+    this.setup_map();
+    return this;
+  },
 
+  setup_map: function() {
+    this.$map_el = this.$('.map-embed');
+    this.map = new google.maps.Map(this.$map_el.get(0), {
+      center: {lat: this.model.get('parameters').latitude, lng: this.model.get('parameters').longitude},
+      scrollwheel: false,
+      zoom: parseInt(this.model.get('parameters').zoom),
+      mapTypeId: google.maps.MapTypeId[this.model.get('parameters').map_type]
+    });
+  }
 
 });
