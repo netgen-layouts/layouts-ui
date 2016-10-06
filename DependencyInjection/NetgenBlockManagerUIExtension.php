7207fbe0f6cbec6ffe436b0832dd78caab8fbbbd
Edi Modrić <edi.modric@gmail.com>
2016-10-06 17:50:16 +0200

    Add Symfony assets config

---
+++ DependencyInjection/NetgenBlockManagerUIExtension.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Netgen\Bundle\BlockManagerUIBundle\DependencyInjection;
+
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Yaml\Yaml;
+
+class NetgenBlockManagerUIExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array $configs
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('framework/assets.xml');
+
+        $prependConfigs = array(
+            'framework/assets.yml' => 'framework',
+        );
+
+        foreach ($prependConfigs as $configFile => $prependConfig) {
+            $configFile = __DIR__ . '/../Resources/config/' . $configFile;
+            $config = Yaml::parse(file_get_contents($configFile));
+            $container->prependExtensionConfig($prependConfig, $config);
+            $container->addResource(new FileResource($configFile));
+        }
+    }
+}
