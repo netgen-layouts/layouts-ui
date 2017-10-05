51b42f2e8392c34b7d5cb9ec053593d020aaa279
Edi Modrić <edi.modric@gmail.com>
2017-10-05 14:44:34 +0200

    Bump version to 0.9.2

--- bundle/Version.php
+++ bundle/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\Bundle\BlockManagerUIBundle;
 
 abstract class Version
 {
-    const VERSION = '0.9.1-DEV';
-    const VERSION_ID = 901;
+    const VERSION = '0.9.2-DEV';
+    const VERSION_ID = 902;
     const MAJOR_VERSION = 0;
     const MINOR_VERSION = 9;
-    const RELEASE_VERSION = 1;
+    const RELEASE_VERSION = 2;
     const EXTRA_VERSION = 'DEV';
 }
