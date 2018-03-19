7e1c2659ff39bdbf458e075f7f9b797f4b27f0d7
Marko Zabcic <gm.marko@gmail.com>
2018-03-19 15:18:09 +0100

    Change layout type

---
+++ app/scripts/pages/layout_change_type.js
@@ -0,0 +1,34 @@
+'use strict';
+var LayoutBasePage = require('./layout_base');
+var LayoutLinkChooserView = require('../views/layout_link_chooser');
+var HeaderView = require('../views/header');
+var ZoneLinkingHeaderView = require('../views/zone_linking_header');
+
+
+
+module.exports = LayoutBasePage.extend({
+  master: 'application',
+  layout: 'layout',
+
+  main: function(){
+    LayoutBasePage.prototype.main.apply(this, arguments);
+    Core.g.layout.load_all_blocks({data: {published: true}});
+
+    Core.state.set({mode: 'choosing', section: 'linking'});
+
+
+    new ZoneLinkingHeaderView({
+      el: '#zone_linking_header'
+    });
+
+    new HeaderView({
+      model: this.base_layout
+    }).render_to('.app-center');
+
+    new LayoutLinkChooserView({
+      collection: Core.g.shared_layouts,
+    }).render_to('.layout-chooser');
+
+    return this;
+  },
+});
