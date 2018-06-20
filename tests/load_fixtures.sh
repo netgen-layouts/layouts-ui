d424aff07960083edc329bbf58d75d055ae7ecc9
Edi Modrić <edi.modric@gmail.com>
2018-06-20 17:50:33 +0200

    Fix path to fixtures

--- tests/load_fixtures.sh
+++ tests/load_fixtures.sh
@@ -2,5 +2,5 @@
 export MYSQL_PWD="$2"
 # mysql -u $1 $3 < tests/fixtures/ez.sql
 
-mysql -u $1 $3 < ../block-manager/lib/Tests/_fixtures/schema/schema.mysql.sql
-mysql -u $1 $3 < ../block-manager/lib/Tests/_fixtures/data.mysql.sql
+mysql -u $1 $3 < ../block-manager/tests/_fixtures/schema/schema.mysql.sql
+mysql -u $1 $3 < ../block-manager/tests/_fixtures/data.mysql.sql
